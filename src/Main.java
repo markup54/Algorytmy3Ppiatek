849d1c92cbd32db31e8c04cf094d4b880e54b7f5
markup54 <marzenakupka@tlen.pl>
2023-09-15 11:35:44 +0200

    haslo

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class Main {
 
     /**
@@ -19,8 +21,41 @@ public class Main {
         return szyfr;
     }
 
+    private static boolean czyHasloZawieraCos(String haslo, String ciag){
+        for (int i = 0; i < ciag.length(); i++) {
+            char litera =ciag.charAt(i);
+            if(haslo.indexOf(litera) >=0){
+                return true;
+            }
+        }
+        return false;
+    }
+    public static String generujHaslo(int dl){
+        String haslo ="";
+
+        String wszystkieZnaki ;
+        String maleLitery="qwertyuiopasdfghjklzxcvbnm";
+        String duzeLitery = "QWERTYUIOPASDFGHJKLZXCVBNM";
+        String znakiSpecjalne = "!@#$%^&*()_+=-/?|><,.";
+        String cyfry = "1234567890";
+        wszystkieZnaki = maleLitery+duzeLitery+znakiSpecjalne+cyfry;
+        for (int i = 0; i < dl; i++) {
+            int losowa = (int) (Math.random()*wszystkieZnaki.length());
+            System.out.println(losowa);
+            haslo = haslo + wszystkieZnaki.charAt(losowa);
+        }
+        boolean czyMalaLitera = czyHasloZawieraCos(haslo,maleLitery);
+        boolean czyDuzaLitera = czyHasloZawieraCos(haslo,duzeLitery);
+        boolean czyZnakSpecjalny = czyHasloZawieraCos(haslo,znakiSpecjalne);
+        boolean czyCyfra = czyHasloZawieraCos(haslo,cyfry);
+        //TODO: dokonczyc
+
+        return haslo;
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println(szyfruj("abc",3));
+        System.out.println(generujHaslo(20));
     }
 }
